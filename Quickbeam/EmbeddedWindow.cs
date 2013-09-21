572f2a3d18f10e4de0943461e2cfa4e583decc9e
Chad Zawistowski <chad.zawistowski@gmail.com>
2013-09-21 00:49:15 -0400

    Custom, dark-themed GUI

---
+++ Quickbeam/EmbeddedWindow.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Quickbeam
+{
+    public class EmbeddedWindow : IDisposable
+    {
+        private Process _process;
+
+        private const int GWL_STYLE = -16;
+        private const int SW_SHOW = 5;
+        private const int SWP_NOACTIVATE = 0x0010;
+        private const int SWP_NOZORDER = 0x0004;
+        private const int WS_CAPTION = 0x00C00000;
+        private const int WS_THICKFRAME = 0x00040000;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool EnableWindow(IntPtr hwnd, bool enabled);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+
+
+        public EmbeddedWindow(Window hostWin, string exePath, string workingDirectory, string arguments, string windowTitle)
+        {
+            WindowInteropHelper wndHelp = new WindowInteropHelper(hostWin);
+            ProcessStartInfo psi = new ProcessStartInfo(exePath);
+            psi.WorkingDirectory = workingDirectory;
+            psi.Arguments = arguments;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            _process = Process.Start(psi);
+
+            // Wait for the child window to invisibly load, then relocate into our window
+            _process.WaitForInputIdle();
+            Thread.Sleep(2000);
+            IntPtr hWnd = FindWindow(null, windowTitle);
+            ShowWindow(hWnd, SW_SHOW);
+            EnableWindow(hWnd, true);
+            SetParent(_process.MainWindowHandle, wndHelp.Handle);
+
+            // remove control box
+            int style = GetWindowLong(_process.MainWindowHandle, GWL_STYLE);
+            style = style & ~WS_CAPTION & ~WS_THICKFRAME;
+            SetWindowLong(_process.MainWindowHandle, GWL_STYLE, style);
+
+            Resize(800, 600);
+        }
+
+        public void Resize(int width, int height, int x = 6, int y = 42)
+        {
+            SetWindowPos(_process.MainWindowHandle, IntPtr.Zero, x, y, width, height, SWP_NOZORDER | SWP_NOACTIVATE);
+        }
+
+        public void Dispose()
+        {
+            if (_process != null)
+            {
+                _process.Refresh();
+                _process.Close();
+            }
+        }
+    }
+}
--- Quickbeam/MainWindow.xaml.cs
+++ Quickbeam/MainWindow.xaml.cs
@@ -1,135 +1,94 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Quickbeam
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
-        private Process _process;
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
-
-        private const int SWP_NOZORDER = 0x0004;
-        private const int SWP_NOACTIVATE = 0x0010;
-        private const int GWL_STYLE = -16;
-        private const int WS_CAPTION = 0x00C00000;
-        private const int WS_THICKFRAME = 0x00040000;
-
-        [Flags]
-        public enum ProcessAccessFlags : uint
-        {
-            All = 0x001F0FFF,
-            Terminate = 0x00000001,
-            CreateThread = 0x00000002,
-            VMOperation = 0x00000008,
-            VMRead = 0x00000010,
-            VMWrite = 0x00000020,
-            DupHandle = 0x00000040,
-            SetInformation = 0x00000200,
-            QueryInformation = 0x00000400,
-            Synchronize = 0x00100000
-        }
-
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr OpenProcess(int dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll")]
-        public static extern Int32 CloseHandle(IntPtr hProcess);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
-
-        //Initialization
-        private static int SW_SHOW = 5;
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
-
-        [DllImport("user32.dll")]
-        private static extern bool EnableWindow(IntPtr hwnd, bool enabled);
+        private EmbeddedWindow _haloWindow;
+        private bool restoreIfMove;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // For whatever reason, the default behavior of a Metro window does
+            // not allow you to 'unsnap' from a maximized state by dragging the
+            // titlebar. This allows such behavior.
+            #region JankyMouseHandlers
+            this.MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.ClickCount == 2)
+                {
+                    if ((ResizeMode == ResizeMode.CanResize) ||
+                        (ResizeMode == ResizeMode.CanResizeWithGrip))
+                    {
+                        SwitchState();
+                    }
+                }
+                else
+                {
+                    if (WindowState == WindowState.Maximized)
+                    {
+                        restoreIfMove = true;
+                    }
+                }
+            };
+
+            this.MouseLeftButtonUp += (s, e) =>
+            {
+                restoreIfMove = false;
+            };
+
+            this.MouseMove += (s, e) =>
+            {
+                if (restoreIfMove)
+                {
+                    restoreIfMove = false;
+                    var mouseX = e.GetPosition(this).X;
+                    var width = RestoreBounds.Width;
+                    var x = mouseX - width / 2;
+
+                    WindowState = WindowState.Normal;
+                    Left = x;
+                    Top = 36;
+                    DragMove();
+                }
+            };
+            #endregion JankyMouseHandlers
+        }
+
+        private void SwitchState()
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+
+            else if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
         }
 
         private void btnHost_Click(object sender, RoutedEventArgs e)
         {
-            WindowInteropHelper wndHelp = new WindowInteropHelper(this);
-            ProcessStartInfo psi = new ProcessStartInfo(@"D:\Program Files (x86)\Microsoft Games\Halo\halo.exe");
-            psi.WorkingDirectory = @"D:\Program Files (x86)\Microsoft Games\Halo\";
-            psi.Arguments = @"-console -window -vidmode 800,600,60";
-            psi.WindowStyle = ProcessWindowStyle.Hidden;
-            _process = Process.Start(psi);
-
-            // Wait for Halo to invisibly load, then move into our window
-            _process.WaitForInputIdle();
-            Thread.Sleep(2000);
-            IntPtr HWND = FindWindow(null, "Halo");
-            ShowWindow(HWND, SW_SHOW);
-            EnableWindow(HWND, true);
-            SetParent(_process.MainWindowHandle, wndHelp.Handle);
-
-            // remove control box
-            int style = GetWindowLong(_process.MainWindowHandle, GWL_STYLE);
-            style = style & ~WS_CAPTION & ~WS_THICKFRAME;
-            SetWindowLong(_process.MainWindowHandle, GWL_STYLE, style);
-
-            // resize embedded application & refresh
-            ResizeEmbeddedApp();
-        }
-
-        private void ResizeEmbeddedApp()
-        {
-            if (_process == null)
-                return;
-
-            SetWindowPos(_process.MainWindowHandle, IntPtr.Zero, 0, 0, 800, 600, SWP_NOZORDER | SWP_NOACTIVATE);
+            _haloWindow = new EmbeddedWindow(this,
+                @"D:\Program Files (x86)\Microsoft Games\Halo\halo.exe",
+                @"D:\Program Files (x86)\Microsoft Games\Halo\",
+                @"-console -window -vidmode 800,600,60",
+                "Halo");
         }
 
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
             base.OnClosing(e);
-            if (_process != null)
+            if (_haloWindow != null)
             {
-                _process.Refresh();
-                _process.Close();
+                _haloWindow.Dispose();
             }
         }
     }
