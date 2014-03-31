9f164078901022c2e5fc69ff7e8e0b643e9a4385
Chad Zawistowski <chad.zawistowski@gmail.com>
2014-03-31 18:03:59 -0400

    Start Halo from a background thread

--- src/MetroIde/Helpers/HaloWindow.cs
+++ src/MetroIde/Helpers/HaloWindow.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Interop;
 
 namespace MetroIde.Helpers
@@ -33,37 +34,46 @@ namespace MetroIde.Helpers
                 IntPtr.Zero,
                 0);
 
-            //TODO async fork here?
-
-            var psi = new ProcessStartInfo(@"D:\Program Files (x86)\Microsoft Games\Halo\halo.exe")
+            // This code uses Thread.Sleep, so finish on a background thread
+            Task.Factory.StartNew(() =>
             {
-                WorkingDirectory = @"D:\Program Files (x86)\Microsoft Games\Halo\",
-                Arguments = string.Format(@"-console -window -vidmode {0},{1},60", 800, 600),
-                WindowStyle = ProcessWindowStyle.Normal
-            };
-            _haloProcess = Process.Start(psi);
-            _haloProcess.WaitForInputIdle();
-            Thread.Sleep(2000);
+                var psi = new ProcessStartInfo(@"D:\Program Files (x86)\Microsoft Games\Halo\halo.exe")
+                {
+                    WorkingDirectory = @"D:\Program Files (x86)\Microsoft Games\Halo\",
+                    Arguments = string.Format(@"-console -window -vidmode {0},{1},60", 800, 600),
+                    WindowStyle = ProcessWindowStyle.Normal
+                };
+                _haloProcess = Process.Start(psi);
+                _haloProcess.WaitForInputIdle();
+                Thread.Sleep(2000);
 
-            // reveal and relocate into our window
-            NativeMethods.ShowWindow(_haloProcess.MainWindowHandle, NativeMethods.SwShow);
-            NativeMethods.SetParent(_haloProcess.MainWindowHandle, _hwndHost);
+                // reveal and relocate into our window
+                NativeMethods.ShowWindow(_haloProcess.MainWindowHandle, NativeMethods.SwShow);
+                NativeMethods.SetParent(_haloProcess.MainWindowHandle, _hwndHost);
 
-            // remove control box
-            int style = NativeMethods.GetWindowLong(_haloProcess.MainWindowHandle, NativeMethods.GwlStyle);
-            style = style & ~NativeMethods.WsCaption & ~NativeMethods.WsThickframe;
-            NativeMethods.SetWindowLong(_haloProcess.MainWindowHandle, NativeMethods.GwlStyle, style);
+                // remove control box
+                int style = NativeMethods.GetWindowLong(_haloProcess.MainWindowHandle, NativeMethods.GwlStyle);
+                style = style & ~NativeMethods.WsCaption & ~NativeMethods.WsThickframe;
+                NativeMethods.SetWindowLong(_haloProcess.MainWindowHandle, NativeMethods.GwlStyle, style);
 
-            // resize
-            NativeMethods.SetWindowPos(_haloProcess.MainWindowHandle, IntPtr.Zero, 0, 0, 800, 600, NativeMethods.SwpNozorder | NativeMethods.SwpNoactivate);
+                // resize
+                NativeMethods.SetWindowPos(_haloProcess.MainWindowHandle, IntPtr.Zero, 0, 0, 800, 600, NativeMethods.SwpNozorder | NativeMethods.SwpNoactivate);
 
-            //TODO force render trick
+                //TODO force render trick
+            });
 
             return new HandleRef(this, _hwndHost);
         }
 
         protected override void DestroyWindowCore(HandleRef hwnd)
         {
+            if (_haloProcess == null) return;
+            _haloProcess.Close();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
             if (_haloProcess == null) return;
             _haloProcess.Close();
         }
