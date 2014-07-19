287cac0db2e043098536e5196a5a534f0ec25c41
Chad Zawistowski <chad.zawistowski@gmail.com>
2014-07-19 13:26:35 -0400

    Embedded window stuff

--- Quickbeam/EmbeddedWindow.cs
+++ Quickbeam/EmbeddedWindow.cs
@@ -8,40 +8,6 @@ using System.Windows.Threading;
 
 namespace Quickbeam
 {
-    internal static class NativeMethods
-    {
-        public const int GWL_STYLE = -16;
-        public const int SW_SHOW = 5;
-        public const int SWP_NOACTIVATE = 0x0010;
-        public const int SWP_NOZORDER = 0x0004;
-        public const int WS_CAPTION = 0x00C00000;
-        public const int WS_THICKFRAME = 0x00040000;
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool EnableWindow(IntPtr hwnd, [MarshalAs(UnmanagedType.Bool)] bool enabled);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
-    }
-
     public sealed class EmbeddedWindow : IDisposable
     {
         private Process _process;
@@ -63,21 +29,21 @@ namespace Quickbeam
             if (hWnd != null && !_process.HasExited)
             {
                 // reveal and relocate into our window
-                NativeMethods.ShowWindow(hWnd, NativeMethods.SW_SHOW);
-                NativeMethods.EnableWindow(hWnd, true);
+                //NativeMethods.ShowWindow(hWnd, NativeMethods.SW_SHOW);
+                //NativeMethods.EnableWindow(hWnd, true);
                 NativeMethods.SetParent(_process.MainWindowHandle, wndHelp.Handle);
 
                 // remove control box
-                int style = NativeMethods.GetWindowLong(_process.MainWindowHandle, NativeMethods.GWL_STYLE);
-                style = style & ~NativeMethods.WS_CAPTION & ~NativeMethods.WS_THICKFRAME;
-                NativeMethods.SetWindowLong(_process.MainWindowHandle, NativeMethods.GWL_STYLE, style);
+                //int style = NativeMethods.GetWindowLong(_process.MainWindowHandle, NativeMethods.GWL_STYLE);
+                //style = style & ~NativeMethods.WS_CAPTION & ~NativeMethods.WS_THICKFRAME;
+                //NativeMethods.SetWindowLong(_process.MainWindowHandle, NativeMethods.GWL_STYLE, style);
             }
             else Dispose();
         }
 
         public void Resize(int width, int height, int xCoor, int yCoor)
         {
-            NativeMethods.SetWindowPos(_process.MainWindowHandle, IntPtr.Zero, xCoor, yCoor, width, height, NativeMethods.SWP_NOZORDER | NativeMethods.SWP_NOACTIVATE);
+            //NativeMethods.SetWindowPos(_process.MainWindowHandle, IntPtr.Zero, xCoor, yCoor, width, height, NativeMethods.SWP_NOZORDER | NativeMethods.SWP_NOACTIVATE);
         }
 
         public void Dispose()
---
+++ Quickbeam/HaloWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace Quickbeam
+{
+    public class HaloWindow : HwndHost
+    {
+        private IntPtr _hwndHost;
+        private Process _haloProcess;
+        readonly int _hostHeight;
+        readonly int _hostWidth;
+
+        public HaloWindow()
+        {
+            _hostHeight = 800;
+            _hostWidth = 600;
+        }
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            _hwndHost = IntPtr.Zero;
+
+            _hwndHost = NativeMethods.CreateWindowEx(
+                0, "static", "",
+                NativeMethods.WsChild | NativeMethods.WsVisible | NativeMethods.WsClipChildren,
+                0, 0,
+                _hostWidth, _hostHeight,
+                hwndParent.Handle,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                0);
+
+            // This code uses Thread.Sleep, so finish on a background thread
+            Task.Factory.StartNew(() =>
+            {
+                var psi = new ProcessStartInfo(@"C:\Program Files (x86)\Microsoft Games\Halo\halo.exe")
+                {
+                    WorkingDirectory = @"C:\Program Files (x86)\Microsoft Games\Halo\",
+                    Arguments = string.Format(@"-console -window -vidmode {0},{1},60", 800, 600),
+                    WindowStyle = ProcessWindowStyle.Minimized
+                };
+                _haloProcess = Process.Start(psi);
+                _haloProcess.WaitForInputIdle();
+                Thread.Sleep(2000);
+
+                // remove control box
+                int style = NativeMethods.GetWindowLong(_haloProcess.MainWindowHandle, NativeMethods.GwlStyle);
+                style = style & ~NativeMethods.WsCaption & ~NativeMethods.WsThickframe;
+                NativeMethods.SetWindowLong(_haloProcess.MainWindowHandle, NativeMethods.GwlStyle, style);
+
+                // reveal and relocate into our window
+                NativeMethods.SetParent(_haloProcess.MainWindowHandle, _hwndHost);
+                NativeMethods.ShowWindow(_haloProcess.MainWindowHandle, NativeMethods.SwShow);
+
+                // resize
+                NativeMethods.SetWindowPos(_haloProcess.MainWindowHandle, IntPtr.Zero, 0, 0, 800, 600, NativeMethods.SwpNoZOrder | NativeMethods.SwpNoActivate);
+
+                //TODO force render trick
+            });
+
+            return new HandleRef(this, _hwndHost);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            if (_haloProcess == null) return;
+            _haloProcess.Close();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (_haloProcess == null) return;
+            _haloProcess.Close();
+        }
+    }
+}
