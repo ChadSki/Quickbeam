dc06f4e5c0956406aa4a3100a7e1ae56b9843f8d
Chad Zawistowski <chad.zawistowski@gmail.com>
2014-09-11 13:33:15 -0700

    Perfect native resizing and Aero Snap

--- src/Quickbeam/Windows/Home.xaml.cs
+++ src/Quickbeam/Windows/Home.xaml.cs
@@ -47,5 +47,47 @@ namespace Quickbeam.Windows
 
 			base.OnSourceInitialized(e);
 		}
+
+        #region Resizing
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private enum ResizeDirection
+        {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            TopLeft = 4,
+            TopRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            BottomRight = 8,
+        }
+
+        private const int WM_SYSCOMMAND = 0x112;
+
+        private void ResizeWindow(ResizeDirection direction)
+        {
+            SendMessage(Helper.Handle, WM_SYSCOMMAND, (IntPtr)(61440 + direction), IntPtr.Zero);
+        }
+
+        private void ResizeTop(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.Top); }
+
+        private void ResizeBottom(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.Bottom); }
+
+        private void ResizeLeft(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.Left); }
+
+        private void ResizeRight(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.Right); }
+
+        private void ResizeTopLeft(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.TopLeft); }
+
+        private void ResizeTopRight(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.TopRight); }
+
+        private void ResizeBottomLeft(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.BottomLeft); }
+
+        private void ResizeBottomRight(object sender, MouseButtonEventArgs e) { ResizeWindow(ResizeDirection.BottomRight); }
+
+        #endregion Resizing
     }
 }
