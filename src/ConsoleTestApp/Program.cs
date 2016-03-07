69327229d9f851e22307801d85b9cb78768d9b85
Chad Zawistowski <chad.zawistowski@gmail.com>
2016-03-06 16:05:27 -0800

    Restrict to x64. Importing Python works.

--- src/ConsoleTestApp/Program.cs
+++ src/ConsoleTestApp/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleTestApp
     {
         static void Main(string[] args)
         {
-            var foo = new Class1();
+            Class1.doThing();
         }
     }
 }
--- src/PythonBinding/PythonBinding.h
+++ src/PythonBinding/PythonBinding.h
@@ -1,13 +1,41 @@
 // PythonBinding.h
 
 #pragma once
+#include "Stdafx.h"
 
 using namespace System;
 
 namespace PythonBinding {
 
+    /*
+    /// Wraps a PyObject known to have a property_changed Event.
+    public class BoundPyObject
+    {
+    public:
+        BoundPyObject(PyObject* po);
+        BoundPyObject(const BoundPyObject &po);
+        ~BoundPyObject();
+
+        /// Triggered whenever a property of the bound PyObject
+        /// is updated.
+        void OnPropertyChanged(char* name);
+
+    private:
+        PyObject* _po;
+    };*/
+
     public ref class Class1
     {
-        // TODO: Add your methods for this class here.
+    public:
+        /// Pseudo-main function for developing purposes.
+        ///
+        static void doThing()
+        {
+            Py_Initialize();
+            PyRun_SimpleString("import sys");
+            PyRun_SimpleString("sys.stdout = open('CONOUT$', 'wt')");
+            PyRun_SimpleString("print('hello')");
+            Py_Finalize();
+        }
     };
 }
