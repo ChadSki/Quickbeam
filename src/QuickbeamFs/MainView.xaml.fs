1930b8c47da787e3b22531f324c2f147a594f533
Chad Zawistowski <chad.zawistowski@gmail.com>
2016-08-14 17:40:53 -0700

    FSharp WPF finally working

---
+++ src/QuickbeamFs/MainView.xaml.fs
@@ -0,0 +1,15 @@
+﻿namespace Views
+
+open FsXaml
+
+// Demonstrates technique for adding "code behind" logic
+// Use a different name for the base class, then inherit to add code behind
+type MainViewBase = XAML<"MainView.xaml">
+
+// Inherited class is MainView, which is referred to/used in MainWindow.xaml directly
+type MainView() =
+    inherit MainViewBase()
+
+    let showMessage _ =
+        System.Windows.MessageBox.Show "You double clicked on Full Name!"
+        |> ignore
---
+++ src/QuickbeamFs/MainWindow.xaml.fs
@@ -0,0 +1,5 @@
+﻿namespace Views
+
+open FsXaml
+
+type MainWindow = XAML<"MainWindow.xaml">
