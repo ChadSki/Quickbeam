4774bb4a523733ec2371b4ff729044410f36bf65
Chad Zawistowski <chad.zawistowski@gmail.com>
2016-10-05 21:09:51 -0700

    HaloFieldNode cleanup

--- src/NimbusSharpGUI/TagEditor/HaloFieldNode.cs
+++ src/NimbusSharpGUI/TagEditor/HaloFieldNode.cs
@@ -1,16 +1,13 @@
 ﻿using ICSharpCode.TreeView;
 using NimbusSharp;
-using System.Linq;
 
 namespace NimbusSharpGUI.TagEditor
 {
     public class HaloFieldNode : SharpTreeNode
     {
-        private HaloField hfield;
-
         public HaloFieldNode(HaloStruct hstruct, HaloField hfield)
         {
-            this.hfield = hfield;
+            Field = hfield;
 
             // Load children eagerly
             var sa = hfield as StructArrayField;
@@ -23,16 +20,14 @@ namespace NimbusSharpGUI.TagEditor
                             new HaloStruct(sa.Children[i]),
                             string.Format("[{0}]", i)));
                 }
+                // Auto-expand if there aren't multiple children
                 if (sa.Children.Count < 2)
                     IsExpanded = true;
             }
         }
 
-        public override object Text { get { return hfield.Name; } }
+        public override object Text { get { return Field.Name; } }
 
-        public HaloField Field
-        {
-            get { return hfield; }
-        }
+        public HaloField Field { get; private set; }
     }
 }
